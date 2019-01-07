7c882a9283efaeb91934cfaf72acf5749f50a0a6
SirWilliamIII <willcarpenterwastaken@gmail.com>
2019-01-06 20:41:51 -0600

    init commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cors());
+
+const todos = [
+  {
+    id: 1,
+    title: "first",
+    description: "description uno"
+  }
+];
+
+app.post("/api/cloudsnap", (req, res) => {
+  const todo = {
+    id: todos.length + 1,
+    title: req.body.title,
+    description: req.body.description
+  };
+
+  todos.push(todo);
+  return res.status(201).send({
+    success: "true",
+    message: "todo added successfully",
+    todo
+  });
+});
+
+app.get("/api/cloudsnap", (req, res) => {
+  res.status(200).send({
+    success: "true",
+    message: "todos retrieved",
+    todos
+  });
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
+});
